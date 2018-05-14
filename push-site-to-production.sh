7808746c40f8713d93b2fffd8502a4c1ce74ff6f
Maxwell Wenger <3391859+maxwenger@users.noreply.github.com>
2018-05-14 00:56:48 -0700

    -MESSAGE

--- push-site-to-production.sh
+++ push-site-to-production.sh
@@ -7,7 +7,7 @@ PRODUCTION-BRANCH='gh-pages'
 
 read -p "Are you sure you want to push this site to production? [yN] " -n 1 -r
 echo # (optional) move to a new line
-if [[ ! $REPLY =~ ^[Yy]$ ]]
+if [[ $REPLY =~ ^[Yy]$ ]]
 then
   docker-compose restart
   cd $JEKYLL-DIR/_site
