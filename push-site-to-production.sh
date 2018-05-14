0b0e9ffb22fb9f19fada8a14c916f6b3e1792dba
Maxwell Wenger <3391859+maxwenger@users.noreply.github.com>
2018-05-14 01:31:47 -0700

    added production script

--- push-site-to-production.sh
+++ push-site-to-production.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
-JEKYLL-DIR=./trophy-jekyll
-COMMIT-MESSAGE="Pushed to production."
-REMOTE-REPOSITORY-URL="https://github.com/maxwenger/maxwenger.github.io"
-PRODUCTION-BRANCH="gh-pages"
-
 read -p "Are you sure you want to push this site to production? [yN] " -n 1 -r
 echo # (optional) move to a new line
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
+  
+  if [ -d $PWD/trophy-jekyll/_site]; then
+    rm -rf $PWD/trophy-jekyll/_site  
+  fi
+
   docker-compose restart
-  cd $JEKYLL-DIR/_site
+  cd $PWD/trophy-jekyll/_site
   git init . &&
+  git remote add origin https://github.com/maxwenger/maxwenger.github.io
+  
+  git fetch &&
   git add . &&
-  git commit -m $COMMIT-MESSAGE &&
-  git remote add origin $REMOTE-REPOSITORY-URL &&
-  git checkout $PRODUCTION-BRANCH &&
-  git push origin $PRODUCTION-BRANCH ||
-  return 1 
+  git commit -m "Pushed to production" &&
+  git push -f origin master 
 fi
